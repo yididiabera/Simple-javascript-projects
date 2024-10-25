d53ae2fbfebc090d3636d9864491189baf2a2da0
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-25 19:27:46 +0300

    Done creating: Pokeman

---
+++ Pokeman/index.js
@@ -0,0 +1,34 @@
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Could not fetch resource!");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data.name))
+//   .catch((error) => console.error(error));
+
+async function fetchData() {
+  try {
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+
+    const imgElement = document.getElementById("pokemonSprite");
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
