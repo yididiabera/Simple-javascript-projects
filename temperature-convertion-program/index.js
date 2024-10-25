a8536b60c76750edbd0e411da5a1d11f25124599
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-25 19:28:06 +0300

    Done creating: temperature-convertion-program

---
+++ temperature-convertion-program/index.js
@@ -0,0 +1,23 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+const btn = document.getElementById("btn");
+
+let temp;
+
+function convert() {
+  if (toFahrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp + "°F";
+  } else if (toCelsius.checked) {
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp + "°C";
+  } else {
+    result.textContent = "Select a unit";
+  }
+}
+
+// btn.onclick = convert;
